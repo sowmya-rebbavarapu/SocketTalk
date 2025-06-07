a116f0aeadbc3591eabe24a2434ddac08c5444b1
sowmya-rebbavarapu <sowmyarebbavarapu@gmail.com>
2025-06-07 13:44:08 +0530

    storing messages and images function

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,5 +1,6 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
 
 // Get all users except the logged-in user, and count unseen messages
 export const getUserForSidebar = async (req, res) => {
@@ -73,3 +74,29 @@ export const markMessageAsSeen = async (req, res) => {
         res.status(500).json({ success: false, message: error.message });
     }
 };
+
+
+//send message to selected usr
+export const sendMessage=async(req,res)=>{
+    try {
+        const {text,image}=req.body;
+        const receiverId=req.params.id;
+        const senderId=req.user._id;
+        let imageUrl;
+        if(image){
+           const uploadResponse=await cloudinary.uploader.upload(image)
+           imageUrl=uploadResponsesecure_url;
+        }
+
+        const newMessage=await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        })
+        res.json({success:true,newMessage});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
