3f1faf222e5156758fdad853815f227f6bb1ed70
sowmya-rebbavarapu <sowmyarebbavarapu@gmail.com>
2025-06-07 14:07:47 +0530

    added socket.io function in server.js

--- server/server.js
+++ server/server.js
@@ -5,8 +5,33 @@ import http from "http"
 import {connectDB} from "./lib/db.js"
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
+import {Server} from "socket.io";
+
+
 const app=express();
 const server=http.createServer(app)
+
+//initialized socket.io server
+export const io=new Server(server,{
+    cors:{origin:"*"}
+})
+
+//store online users
+export const userSocketMap={};
+
+//socket.io connection handler
+io.on("connection",(socket)=>{
+    const userId=socket.handshake.query.userId;
+    console.log("User Connected",userId);
+    if(userId)userSocketMap[userId]=socket.id;
+    //emit online users to all connected client
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected",userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+    })
+})
 
 //middleware
 app.use(express.json({limit:"4mb"}));
